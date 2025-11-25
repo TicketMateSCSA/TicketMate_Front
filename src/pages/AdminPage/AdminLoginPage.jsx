b1b2afe066724b585755b50f99add013e34303dc
Vida0822 <sinhimin11@naver.com>
2025-11-25 14:09:16 +0900

    feat: 관리자 로그인 페이지 완성

--- src/pages/AdminPage/AdminLoginPage.jsx
+++ src/pages/AdminPage/AdminLoginPage.jsx
@@ -3,9 +3,32 @@ import Navigator from "../../components/Navigator/Navigator";
 
 function AdminLoginPage(){
     return (
-        <div className='detail-page-container'>
-            <button onClick={() => window.history.back()}>뒤로가기</button>
-            <h1>Admin Login Page</h1>
+        <div class="admin-login-container">
+            <div class="admin-box">
+
+                <h1 class="admin-title">ADMIN LOGIN</h1>
+                <p class="admin-sub">Management System Access</p>
+
+                <div class="admin-form-group">
+                    <label>ID</label>
+                    <input type="text" class="admin-input" placeholder="관리자 ID 입력"/>
+                </div>
+
+                <div class="admin-form-group">
+                    <label>Password</label>
+                    <input type="password" class="admin-input" placeholder="비밀번호 입력"/>
+                </div>
+
+                <div class="admin-form-group">
+                    <label>고유 KEY</label>
+                    <input type="text" class="admin-input" placeholder="고유 KEY 입력"/>
+                </div>
+
+                <button class="admin-login-btn">로그인</button>
+
+                <p class="admin-warn">※ 허가된 관리자만 접근 가능합니다.</p>
+
+            </div>
         </div>
     )
 }
