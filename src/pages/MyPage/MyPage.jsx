dce6166d79882cc60a70338c4054cd94f3869df1
suhyeon-gwon <yhus110@snu.ac.kr>
2025-11-25 19:01:02 +0900

    승인, 거절하기 빼고 완료. 403 에러 뜸

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -7,15 +7,14 @@ import { useAuth } from "../../contexts/AuthContext";
 import noProfile from '../../assets/images/no-profile.png';
 
 const myPostURL = `${import.meta.env.VITE_POSTS_URL}/requests/recieved`;
-const myDelURL = `${import.meta.env.VITE_POSTS_URL}/post`;
+const myGetURL = `${import.meta.env.VITE_POSTS_URL}/requests/applicants`;
+const myRegistURL = `${import.meta.env.VITE_POSTS_URL}/requests/sent`;
 
 const ageMap = {0: '10대', 1: '20대', 2: '30대', 3: '40대', 4: '50대+'};
 const genderMap = {0: '무관', 1: '남성', 2: '여성'};
 
 const stateMapRev = {0: "전체", 1: "모집중", 2: "모집완료"};
-const ageMapRev = {0: '10대', 1: '20대', 2: '30대', 3: '40대', 4: '50대+'}
-const genderMapRev = {0: '무관', 1: '남성', 2: '여성'}
-
+const dbStateMapRev = {0: "대기중", 1: "승인됨", 2: "거절됨"}
 
 function MyPageSectionMyPost({item}){
     const navigate = useNavigate();
@@ -91,41 +90,195 @@ function MyPageSectionMyPost({item}){
     )
 }
 
+function MyPageSectionGet({item}){
+    const handleOK = async () => {
+        if (!window.confirm("정말 승인할까요?")) return;
+
+        try {
+            const response = await fetch(`${import.meta.env.VITE_POSTS_URL}/requests/${item.req_id}/status`, {
+                method: "PATCH",
+            });
+
+            if (!response.ok) {
+                const errorText = await response.text();
+                alert(`승인 실패: ${errorText}`);
+                return;
+            }
+
+            alert("승인되었습니다!");
+            window.location.reload();
+            
+
+        } catch (err) {
+            console.error(err);
+            alert("서버 오류로 승인하지 못했습니다.");
+        }
+    }
+
+    const handleNO = async () => {
+        if (!window.confirm("정말 거절할까요?")) return;
+
+        try {
+            const response = await fetch(`${import.meta.env.VITE_POSTS_URL}/requests/${item.req_id}/status`, {
+                method: "PATCH",
+                credentials: "include",
+            });
+
+            if (!response.ok) {
+                const errorText = await response.text();
+                alert(`거절 실패: ${errorText}`);
+                return;
+            }
+
+            alert("거절되었습니다!");
+            window.location.reload();
+            
+
+        } catch (err) {
+            console.error(err);
+            alert("서버 오류로 승인하지 못했습니다.");
+        }
+    }
+
+    return (
+        <div className="myPageMyPost-section-func" >
+            <div>
+            <div class="title-wrapper">
+            <p className="sftitle">{item.mate_title}</p>
+            {item.req_stat == 0 && (
+                <div>
+                    <span className="sfOK" onClick={handleOK}>승인하기</span>
+                    <span className="sfNO" onClick={handleNO}>거절하기</span>
+                </div>
+            )}
+
+            {item.req_stat == 1 && (
+                <div>
+                    <span className="sfOKState">승인완료</span>
+                </div>
+            )}
+
+            {item.req_stat == 2 && (
+                <div>
+                    <span className="sfNOState">거절완료</span>
+                </div>
+            )}
+            </div>
+            <p className="sfaccount">
+            <span style={{fontWeight:"bold"}}>닉네임</span>&nbsp;&nbsp;&nbsp;&nbsp;{item.mem_nn? item.mem_nn : item.mem_name}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{fontWeight:"bold"}}>성별</span>&nbsp;&nbsp;&nbsp;&nbsp;{item.mem_gender? genderMap[item.mem_gender]: "-"}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{fontWeight:"bold"}}>연령</span>&nbsp;&nbsp;&nbsp;&nbsp;{item.mem_age? item.mem_age: "- "}세
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{fontWeight:"bold"}}>메이트 온도</span>&nbsp;&nbsp;&nbsp;&nbsp;{item.mem_score}°</p>
+            <p className="sfmsg">신청 메시지</p>
+            <div className="sfmsgreal">{item.req_msg}</div>
+                
+            </div>
+
+            
+            
+        </div>
+    )
+}
+
+function MyPageSectionRegist({item}){
+    const navigate = useNavigate();
+
+    const dateNtime = item.mate_view_date ? item.mate_view_date.split("T") : [];
+    dateNtime[0] = dateNtime[0].split("-").splice(0, 3);
+    dateNtime[0] = dateNtime[0][0] + "년 " + dateNtime[0][1] + "월 " + dateNtime[0][2] + "일 "
+    dateNtime[1] = dateNtime[1].split(":").slice(0, 2).join(":");
+    const slicedDate = dateNtime.slice(0, 2).join(" ");
+
+    // 버튼 클릭
+    const handleClick = async (item) => {
+        navigate(`/detail/${item.mate_post_id}`);  
+    }
+
+    return (
+        
+        <div className="myPageMyPost-section-func" >
+            <img className="myPageMyPost-section-img" 
+                src={item.perf_img_url ? item.perf_img_url : noImage}
+                alt={item.perf_name}/>
+
+            <div className="myPageMyPost-section-content">
+                <div class="post-header">
+                <span className="myPageMyPost-section-category">{item.cat_name}</span> <span className="myPageMyPost-section-state"
+                    style={
+                        {backgroundColor: item.mate_status === 1 ? "#A1FFA6" : "#E0E0E0",
+                        borderColor: item.mate_status === 1 ? "#40A646" : "#656565",
+                        color: item.mate_status === 1? "#40A646" : "#656565"
+                    }}>
+                    {stateMapRev[item.mate_status]}
+                </span>
+
+                <span onClick={() => handleClick(item)} className="toDetailBtn">상세</span>
+                <span className="db-state" style={
+                        {backgroundColor: "white",
+                        borderColor: item.req_stat === 1 ? "#40A646" : item.req_stat === 0? "#656565" : "red",
+                        color: item.req_stat === 1? "#40A646" : item.req_stat === 0? "#656565 " : "red"
+                    }}>
+                    {dbStateMapRev[item.req_stat]}
+                </span>
+                </div>
+                <p className="myPageMyPost-section-title">{item.mate_title}</p>
+                
+                <table className="mpmp-table">
+                    <tbody>
+                        <tr className="myPageMyPost-section-body1"><td style={{width: "53%"}}>공연: {item.perf_name}</td><td style={{width: "47%"}}>장소: {item.perf_loc}</td></tr>
+                        <tr className="myPageMyPost-section-body1"><td style={{width: "53%"}}>일시: {slicedDate}</td><td style={{width: "47%"}}>모집 인원: {item.mate_num_of_need} (현재 {item.mate_num_of_confirmed}/{item.mate_num_of_need})</td></tr>
+                    </tbody>
+                </table>
+                    
+                
+            </div>
+        </div>
+    )
+
+}
+
 function MyPageMyPosts({myPost}){
-    console.log(myPost);
+    // console.log(myPost);
     return (
         <div>
             {myPost?.length > 0 && myPost.map((item, idx) => (
                 
                 <MyPageSectionMyPost key={`mpmp${idx}`} item={item} />
             ))}
+            {myPost?.length == 0 && (
+                <p style={{textAlign:"center"}}>내가 모집한 메이트가 없습니다.</p>
+            )}
         </div>
     )
 }
 
-function MyPageGet(){
+function MyPageGet({myGet}){
     return (
-        <div style={{ padding: "10px", color: "green" }}>
-                <h3>내가 받은 신청</h3>
-                <ul>
-                <li>신청자 X</li>
-                <li>신청자 Y</li>
-                </ul>
-            </div>
+        <div>
+            {myGet?.length > 0 && myGet.map((item, idx) => (
+                <MyPageSectionGet key={`mg${idx}`} item={item} />
+            ))}
+            {myGet?.length == 0 && (
+                <p style={{textAlign:"center"}}>내가 받은 신청이 없습니다.</p>
+            )}
+        </div>
     )
 }
 
-function MyPageRegist(){
+function MyPageRegist({myRegist}){
     return (
-        <div style={{ padding: "10px", color: "orange" }}>
-                <h3>내가 신청한 메이트</h3>
-                <ul>
-                <li>메이트 M</li>
-                <li>메이트 N</li>
-                </ul>
-            </div>
+        <div>
+            {myRegist?.length > 0 && myRegist.map((item, idx) => (
+                
+                <MyPageSectionRegist key={`mpr${idx}`} item={item} />
+            ))}
+            {myRegist?.length == 0 && (
+                <p style={{textAlign:"center"}}>내가 신청한 메이트가 없습니다.</p>
+            )}
+        </div>
     )
 }
+
 function MyPage(){
     const [myPostList, setMyPost] = useState([]);
     const [myGetList, setMyGet] = useState([]);
@@ -138,8 +291,7 @@ function MyPage(){
     const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null); 
     
-    
-
+    // 1. 내가 모집한 메이트 불러오기
     useEffect(() => {
         fetch(myPostURL)
             .then((response) => {
@@ -168,6 +320,66 @@ function MyPage(){
                 setLoading(false);
             });
     }, []);
+
+
+    // 2. 내가 받은 신청 불러오기
+    useEffect(() => {
+        fetch(myGetURL)
+            .then((response) => {
+                if (!response.ok) {
+                    // setContent(<p style={{textAlign:"center"}}>내가 받은 신청이 없습니다.</p>)
+                    return null;
+                }
+                // console.log(response);
+                return response.json();
+            })
+            .then((data) => {
+                const fetchedPosts = data?.result || [];
+                setMyGet(fetchedPosts);
+                setLoading(false);
+
+            if (fetchedPosts.length > 0) {
+                 setContent(<MyPageGet  myPost={fetchedPosts}/>);
+            } else {
+                //  setContent(<p style={{textAlign:"center"}}>내가 받은 신청이 없습니다.</p>);
+            }
+
+            })
+            .catch((error) => {
+                setError(error.message);
+                setLoading(false);
+            });
+    }, []);
+
+
+    // 3. 내가 신청한 메이트 불러오기
+    useEffect(() => {
+        fetch(myRegistURL)
+            .then((response) => {
+                if (!response.ok) {
+                    // setContent(<p style={{textAlign:"center"}}>내가 신청한 메이트가 없습니다.</p>)
+                    return null;
+                }
+                // console.log(response);
+                return response.json();
+            })
+            .then((data) => {
+                const fetchedPosts = data?.result || [];
+                setMyRegist(fetchedPosts);
+                setLoading(false);
+
+            if (fetchedPosts.length > 0) {
+                 setContent(<MyPageRegist  myPost={fetchedPosts}/>);
+            } else {
+                //  setContent(<p style={{textAlign:"center"}}>내가 신청한 메이트가 없습니다.</p>);
+            }
+
+            })
+            .catch((error) => {
+                setError(error.message);
+                setLoading(false);
+            });
+    }, []);
     
 
     const handleClick = (idx) => {
@@ -177,10 +389,10 @@ function MyPage(){
             setContent(<MyPageMyPosts  myPost={myPostList}/>);
             break;
         case 1:
-            setContent(<MyPageGet/>);
+            setContent(<MyPageGet myGet={myGetList}/>);
             break;
         case 2:
-            setContent(<MyPageRegist/>);
+            setContent(<MyPageRegist myRegist={myRegistList}/>);
             break;
         default:
             setContent(<p style={{textAlign: "center"}}>로딩 중 ...</p>);
@@ -222,6 +434,7 @@ function MyPage(){
         bgc = "#d40f0f";
     }
 
+    const textLen = [myPostList.length, myGetList.length, myRegistList.length]; 
     return(
         <div className="my-page-container">
             <Navigator />
@@ -278,7 +491,7 @@ function MyPage(){
                                     key={idx}
                                     className={activeIdx === idx ? "mr-boxno active" : "mr-boxno"}
                                     onClick={() => handleClick(idx)}>
-                                    <p>{myPostList.length}</p>
+                                    <p>{textLen[idx]}</p>
                                     {text}
                                 </td>
                                 )
